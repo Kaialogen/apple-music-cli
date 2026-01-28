a01e480b8e1009c7c0be61f1c1dee474638d477e
Kaialogen <kaialogen@protonmail.com>
2026-01-28 17:08:54 +0000

    fix: Made write_songs_to_csv universal

--- src/cli/file_output.py
+++ src/cli/file_output.py
@@ -14,37 +14,15 @@ def write_songs_to_json(
     )
 
 
-def write_songs_to_csv(payload: dict, output_file: str = "output/output.csv") -> None:
-    songs: list[dict] = []
-
-    for item in payload.get("data", []):
-        attributes = item.get("attributes", {})
-        if not attributes:
-            continue
-
-        song = {
-            "name": attributes.get("name"),
-            "artistName": attributes.get("artistName"),
-            "albumName": attributes.get("albumName"),
-            "genreNames": attributes.get("genreNames", []),
-            "releaseDate": attributes.get("releaseDate"),
-        }
-
-        songs.append(song)
-
-    # Define the field names
-    field_names: list[str] = [
-        "name",
-        "artistName",
-        "albumName",
-        "genreNames",
-        "releaseDate",
-    ]
+def write_songs_to_csv(
+    payload: List[Dict], output_file: str = "output/output.csv"
+) -> None:
+    dictionary = payload[0]
 
     # Write to CSV file
     path = Path(output_file)
     path.parent.mkdir(parents=True, exist_ok=True)
     with path.open("w", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=field_names)
+        writer = csv.DictWriter(file, fieldnames=dictionary.keys())
         writer.writeheader()
-        writer.writerows(songs)
+        writer.writerows(payload)
